f8a4dd9dff3a28d9cf40788be699597af89e1f58
Lesja01 <Lesja01@mail.ru>
2019-02-23 17:31:10 +0300

    fix: add solution for functionmakeExange

--- src/index.js
+++ src/index.js
@@ -1,5 +1,32 @@
-// PLEASE DON'T change function name
-module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+
+ module.exports=function makeExchange(currency) {
+     let num=currency;
+     let result={};
+     if(num>10000){
+         result= {error: "You are rich, my friend! We don't have so much coins for exchange"}        
+        }else if(num>0){
+            var H=Math.floor (num/50);
+            if (H!=0){result={"H": H};};
+            num=num-50*H;
+            var Q=Math.floor(num/25);
+            if (Q!=0){
+                Object.assign(result, {"Q": Q});
+                };
+            num=num-25*Q;
+            var D=Math.floor(num/10);
+            if (D!=0){result={...result,"D": D}};
+            num=num-10*D;
+            var N=Math.floor(num/5);
+            if (N!=0){result={...result,"N": N}};
+            num=num-5*N;
+            var P=Math.floor(num/1);
+            if (P!=0){result={...result,"P": P}};
+        }else {
+            result={};
+        }
+        
+     return result;
+    
 }
+
+
---
+++ src/tempCodeRunnerFile.js
@@ -0,0 +1,4 @@
+
+            var Q=Math.floor(num%50/25);
+            if (Q!=0){result.push("Q:" Q)};
+            var D=Math.floor(num%25/10);
